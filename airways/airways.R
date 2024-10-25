b8605d1319a148cb5863376ab5437b45b460d45c
ignatiusm <ignatius.menzies@gmail.com>
2024-10-25 17:51:36 +1300

    remove dots in object name

--- airways/airways.R
+++ airways/airways.R
@@ -25,6 +25,10 @@ library(SummarizedExperiment)
 min.cpm <- 1
 min.cpm.fraction <- 1/4
 
+# Output results/save directory
+save.dir <- "results/"
+dir.create(save.dir)
+
 # Data preparation ----
 ## Load and inspect experiment data ----
 
@@ -141,7 +145,7 @@ boxplot(log2(1+filteredCountsMatUQ), las=2, cex.axis=0.5, cex=0.5,
         ylab="log2(1+counts)")
 
 # Output: table of prepared gene expression data (counts x samples)
-write.csv(filteredCountsMatUQ, file = "/Users/iggy/projects/reproducible-r-workshop/filteredCountsMatUQ.csv")
+write.csv(filteredCountsMatUQ, file = paste0(save.dir,format(Sys.time(), "%Y-%m-%dT%H-%M-%S"), "_","filteredCountsMatUQ.csv"))
 
 # Exploratory plots to view the amount of data collected in each sample:
 # Plot the depth of sequencing per sample
