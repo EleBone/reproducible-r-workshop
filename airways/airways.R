716b342085b3e5789de255d540ca2cbe06a998b1
ignatiusm <ignatius.menzies@gmail.com>
2024-10-26 16:45:43 +1300

    Format cpm constants

--- airways/airways.R
+++ airways/airways.R
@@ -19,12 +19,6 @@ library(limma)
 library(ggrepel)
 library(SummarizedExperiment)
 
-# Analysis variables ----
-
-# Gene filtering
-min.cpm <- 1
-min.cpm.fraction <- 1/4
-
 # Data preparation ----
 ## Load and inspect experiment data ----
 
@@ -58,8 +52,8 @@ str(raw.counts.matrix)
 cpm <- cpm(raw.counts.matrix)
 dim(cpm)
 
-# Require genes have at least 'min.cpm' in at least 'min.cpm.fraction' of the samples
-keepGenes <- rowSums(cpm >= min.cpm) >= ncol(cpm) * min.cpm.fraction
+# Require genes have at least 1 record in at least 1/4 of the samples
+keepGenes <- rowSums(cpm >= 1) >= ncol(cpm) * 1/4
 sum(keepGenes)
 filteredCountsMat <- data.matrix(raw.counts.matrix[keepGenes,])
 dim(filteredCountsMat)
