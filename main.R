3828d1ccc249ede7ce7a446cf5b8df1165a2df96
ignatiusm <ignatius.menzies@gmail.com>
2024-11-15 20:34:01 +1300

    Add constants to helpers

--- main.R
+++ main.R
@@ -2,9 +2,11 @@
 
 # Optionally have an argument that removes the results directory
 # for a clean slate run, using
-# unlink("results", recursive = TRUE)
+unlink("results", recursive = TRUE)
 
+source("src/helpers.R")
 source("src/load_data.R")
 source("src/data_prep.R")
 source("src/dge_analysis.R")
 source("src/plots.R")
+rmarkdown::render("src/report.Rmd", output_dir = save_dir)
--- src/helpers.R
+++ src/helpers.R
@@ -1,4 +1,9 @@
+# Constants used in several files
+
 # Output results/save directory
 save_dir <- "results/"
 dir.create(save_dir)
 
+# Gene filtering 
+min.cpm <- 1
+min.cpm.fraction <- 1/4
